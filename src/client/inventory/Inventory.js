8e8e18c4551ec15e2109c401c5f093c11ee41aa4
Hiro <skinner.daniel2004@gmail.com>
2022-04-22 22:59:46 -0600

    Added hotbar + mv from inv to slots/hotbar

--- src/client/inventory/Inventory.js
+++ src/client/inventory/Inventory.js
@@ -7,52 +7,71 @@ import "./Inventory.css";
 export default function Inventory({ inventory, profile, shown }) {
     if (!inventory)
         inventory = new Array(64).fill({ id: 0, count: 0, img: "" });
-    else if(inventory.length < 64){
+    else if (inventory.length < 64) {
         inventory = inventory.concat(new Array(64 - inventory.length).fill({ id: 0, count: 0, img: "" }));
     }
 
     if (!profile)
         profile = {};
 
+    if (!profile.hotbar)
+        profile.hotbar = [];
+
+    if (profile.hotbar.length < 9) {
+        profile.hotbar = profile.hotbar.concat(new Array(9 - profile.hotbar.length).fill({ id: 0, count: 0, img: "" }));
+    }
+
     let [active, setActive] = useState({});
 
     return (
-        <div className={`inventory ${shown ? "" : "hidden"}`}>
-            <div className="inventory-profile">
-                <div className="inventory-profile-name">
-                    <span>{profile.name || "No Name"}</span>
-                    <span>Lvl. {profile.level || 1}</span>
-                </div>
-                <div className="inventory-profile-build">
-                    <div className="inventory-profile-top">
-                        <InventoryItem item={profile.slots && profile.slots.leftHand} slot={1} />
-                        <InventoryItem item={profile.slots && profile.slots.head} slot={2} />
-                        <InventoryItem item={profile.slots && profile.slots.backPiece} slot={3} />
-                        <InventoryItem item={profile.slots && profile.slots.legs} slot={4} />
+        <div className="inv">
+            <div className={`inventory ${shown ? "" : "hidden"}`}>
+                <div className="inventory-profile">
+                    <div className="inventory-profile-name">
+                        <span>{profile.name || "No Name"}</span>
+                        <span>Lvl. {profile.level || 1}</span>
                     </div>
-                    <div className="inventory-profile-center">
-                        <div className="inventory-profile-center-container">
-                            <CharacterBuild profile={profile} />
+                    <div className="inventory-profile-build">
+                        <div className="inventory-profile-top">
+                            <InventoryItem item={profile.slots && profile.slots.leftHand} active={active} setActive={setActive} slot={1} />
+                            <InventoryItem item={profile.slots && profile.slots.head} active={active} setActive={setActive} slot={2} />
+                            <InventoryItem item={profile.slots && profile.slots.backPiece} active={active} setActive={setActive} slot={3} />
+                            <InventoryItem item={profile.slots && profile.slots.legs} active={active} setActive={setActive} slot={4} />
+                        </div>
+                        <div className="inventory-profile-center">
+                            <div className="inventory-profile-center-container">
+                                <CharacterBuild profile={profile} />
+                            </div>
+                        </div>
+                        <div className="inventory-profile-bottom">
+                            <InventoryItem item={profile.slots && profile.slots.rightHand} active={active} setActive={setActive} slot={5} />
+                            <InventoryItem item={profile.slots && profile.slots.chest} active={active} setActive={setActive} slot={6} />
+                            <InventoryItem item={profile.slots && profile.slots.feet} active={active} setActive={setActive} slot={7} />
                         </div>
                     </div>
-                    <div className="inventory-profile-bottom">
-                        <InventoryItem item={profile.slots && profile.slots.rightHand} slot={5} />
-                        <InventoryItem item={profile.slots && profile.slots.chest} slot={6} />
-                        <InventoryItem item={profile.slots && profile.slots.feet} slot={7} />
-                    </div>
+                </div>
+                <div className="inventory-items">
+                    <ul>
+                        {inventory.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <InventoryItem item={item} active={active} setActive={setActive} />
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
-            <div className="inventory-items">
-                <ul>
-                    {inventory.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <InventoryItem item={item} active={active} setActive={setActive} />
-                            </li>
-                        );
-                    })}
-                </ul>
+            <div className={`hotbar ${shown ? "" : "solo"}`}>
+                {profile.hotbar.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <InventoryItem item={item} active={active} setActive={setActive} hotbar={index} />
+                        </div>
+                    )
+                })}
             </div>
         </div>
+
     );
 }
--- src/client/inventory/InventoryItem/InventoryItem.js
+++ src/client/inventory/InventoryItem/InventoryItem.js
@@ -3,7 +3,7 @@ import { BlockIndex } from "../../../game/Indexes/BlockIndex";
 
 import "./InventoryItem.css";
 
-export default function InventoryItem({ item, active, setActive, slot }) {
+export default function InventoryItem({ item, active, setActive, slot, hotbar }) {
 
     let iName = "";
 
@@ -13,44 +13,37 @@ export default function InventoryItem({ item, active, setActive, slot }) {
         iName = BlockIndex.blocks[item.id];
     }
 
-    if (slot)
-        return (
-            <div className="inventory-slot" id={`slot${slot}`}>
-                <div className="inventory-item-image">
-                    {iName && iName.trim().length > 0 ? (
-                        <img src={`assets/blocks/${iName}.png`} alt="slotItem" />
-                    ) : (
-                        <div className="inventory-item-image-empty">
-                        </div>
-                    )}
-                </div>
-                <div className="inventory-item-count">
-                    {item.count && item.count > 0 ? (
-                        <span>{item.count}</span>
-                    ) : (
-                        <span></span>
-                    )}
-                </div>
+    return (
+        <div className="inventory-slot" id={slot ? `slot${slot}` : hotbar ? `hotbar${hotbar}` : null} onClick={() => handleCurrent(active, setActive, item, slot, hotbar)}>
+            <div className={`${active && active.id && active.id == item.id && active.id != 0 ? "inv-selected" : ""}`}></div>
+            <div className="inventory-item-image">
+                {iName && iName.trim().length > 0 ? (
+                    <img src={`assets/blocks/${iName}.png`} alt="slotItem" />
+                ) : (
+                    <div className="inventory-item-image-empty">
+                    </div>
+                )}
             </div>
-        )
-    else
-        return (
-            <div className={`inventory-item ${active.id == (item.id || 0) ? "glow" : ""}`} onClick={() => setActive(item)}>
-                <div className="inventory-item-image">
-                    {iName && iName.trim().length > 0 ? (
-                        <img src={`assets/blocks/${iName}.png`} alt="slotItem" />
-                    ) : (
-                        <div className="inventory-item-image-empty">
-                        </div>
-                    )}
-                </div>
-                <div className="inventory-item-count">
-                    {item.count && item.count > 0 ? (
-                        <span>{item.count}</span>
-                    ) : (
-                        <span></span>
-                    )}
-                </div>
+            <div className="inventory-item-count">
+                {item.count && item.count > 0 ? (
+                    <span>{item.count}</span>
+                ) : (
+                    <span></span>
+                )}
             </div>
-        )
+        </div>
+    )
+}
+
+function handleCurrent(active, setActive, item, slot, hotbar) {
+    if (slot) {
+        console.log("Selected slot");
+    } else if (hotbar) {
+        console.log(`Selected hotbar`);
+    } else {
+        console.log("Selected item");
+    }
+
+    if (item && item.id != 0)
+        setActive({ id: item.id });
 }
